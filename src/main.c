8d3d6c254ecc1f25669fbd013b98b68c025dd091
RomanDonw <romandonw628@gmail.com>
2026-04-06 21:16:35 +1000

    updated main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <json-c/json.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "util/readfulltextfile.h"
+
+char *readfile(const char *filename);
 
 int main(int argc, char **argv)
 {
+    char *configfname = NULL;
+    
     {
         int p;
         while ((p = getopt(argc, argv, "c:")) != -1)
@@ -10,11 +20,28 @@ int main(int argc, char **argv)
             switch (p)
             {
                 case 'c':
-                    puts(optarg);
+                    configfname = optarg;
                     break;
             }
         }
     }
+
+    if (!configfname)
+    {
+        puts("Missed required parameter \"-c\" - config .json file path.");
+        return 1;
+    }
+
+    char *configcontent = readfulltextfile(configfname);
+    if (!configcontent)
+    {
+        printf("Can't open file \"%s\".\n", configfname);
+        return 1;
+    }
+
+    puts(configcontent);
+
+    free(configcontent);
 
     return 0;
 }
